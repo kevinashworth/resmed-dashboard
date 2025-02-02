c7c25313b95e28b6c53eeab47bd1e1907e35faac
Kevin Ashworth <kevinashworth@yahoo.com>
2025-02-02 14:29:53 -0800

    Add a portion of LayerChart Bar Chart
    just for testing some integration and CSS stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 <nav class="flex gap-4 bg-amber-50">
   <a class="m-1 p-1 hover:underline" href="/">Home</a>
   <a class="m-1 p-1 hover:underline" href="/sleep-data">Sleep Data</a>
+  <a class="m-1 p-1 hover:underline" href="/barchart">Bar Chart</a>
 </nav>
 
 <main class="m-1 p-1">
